cda321d2c1693bb38d6d0ca9dc417d2b3b263716
Paweł Szewczyk <Pawel.Szewczyk@zstio.edu.pl>
2022-12-16 21:21:57 +0100

    Kantor

---
+++ Projekt/Projekt/ClassModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt
+{
+    class ClassModel
+    {
+        public class Info
+        {
+            public int timestamp { get; set; }
+            public double rate { get; set; }
+        }
+
+        public class Query
+        {
+            public string from { get; set; }
+            public string to { get; set; }
+            public int amount { get; set; }
+        }
+
+        public class Root
+        {
+            public bool success { get; set; }
+            public Query query { get; set; }
+            public Info info { get; set; }
+            public string date { get; set; }
+            public double result { get; set; }
+        }
+
+    }
+}
---
+++ Projekt/Projekt/Kantor.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static Projekt.ClassModel;
+
+namespace Projekt
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Kantor : ContentPage
+	{
+		List <string> Waluty = new List<string>();
+		public Kantor ()
+		{
+			InitializeComponent ();
+			Waluty.Add("PLN");
+			Waluty.Add("EUR");
+			Waluty.Add("GBP");
+			Waluty.Add("USD");
+			Waluty.Add("CZK");
+            Picker.ItemsSource = Waluty;
+            Picker2.ItemsSource = Waluty;
+        }
+
+        private async void Exchange(object sender, EventArgs e)
+        {
+			if(Picker.SelectedIndex >=0 && Picker2.SelectedIndex >= 0 && Value.Text != null && Value.Text !=string.Empty) 
+			{
+                Currency.TextColor = Xamarin.Forms.Color.Blue;
+				Currency.Text = "Zamiana walut, proszę czekać";
+				Currency.FontSize = 25;
+                string valut1 = Waluty[Picker.SelectedIndex];
+				string valut2 = Waluty[Picker2.SelectedIndex];
+				string value = Value.Text;
+				string request = "https://api.apilayer.com/exchangerates_data/convert?to="+valut2+"&from="+valut1+"&amount="+value+"&apikey=HraVKaXnOFrP15XQkrOpGgSLiJxqKdM7";
+				try
+				{
+                    HttpClient client = new HttpClient();
+                    var json = await client.GetStringAsync(request);
+                    Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(json);
+
+                    Currency.TextColor = Xamarin.Forms.Color.Black;
+                    Currency.FontSize = 40;
+                    Currency.Text = Math.Round(myDeserializedClass.result, 2).ToString() + " " + valut2;
+                }
+				catch (Exception)
+				{
+                          
+                    Currency.FontSize = 40;
+                    Currency.TextColor = Xamarin.Forms.Color.Red;
+                    Currency.Text = "Błąd połączenia!";
+                }
+                		
+            }
+			else if(Picker.SelectedIndex < 0 || Picker2.SelectedIndex < 0)
+			{
+                Currency.FontSize = 40;
+                Currency.TextColor = Xamarin.Forms.Color.Red;
+				Currency.Text = "Nie podano walut!";
+            }
+			else if(Value.Text == null || Value.Text == string.Empty)
+			{
+                Currency.FontSize = 40;
+                Currency.TextColor = Xamarin.Forms.Color.Red;
+                Currency.Text = "Nie podano ilosci!";
+            }
+
+        }
+    }
+}
