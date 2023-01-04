ac1828aeb6628e992ab8043a4faa4b20da83d128
PSzew <119743585+PSzew@users.noreply.github.com>
2023-01-04 16:39:08 +0100

    Update Kantor.xaml.cs
    komentarze

--- Projekt/Projekt/Kantor.xaml.cs
+++ Projekt/Projekt/Kantor.xaml.cs
@@ -16,46 +16,46 @@ namespace Projekt
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Kantor : ContentPage
 	{
-		List <string> Waluty = new List<string>();
+		List <string> Waluty = new List<string>(); //Zadeklarowanie listy walut
 		public Kantor ()
 		{
 			InitializeComponent ();
-			Waluty.Add("PLN");
+			Waluty.Add("PLN"); //przypisanie liście wartości
 			Waluty.Add("EUR");
 			Waluty.Add("GBP");
 			Waluty.Add("USD");
 			Waluty.Add("CZK");
-            Picker.ItemsSource = Waluty;
+            Picker.ItemsSource = Waluty; //ustawienie data bindingu pickerów na liste walut
             Picker2.ItemsSource = Waluty;
         }
 
-        private async void Exchange(object sender, EventArgs e)
+        private async void Exchange(object sender, EventArgs e) //Asynchroniczna funkcja do przeliczenia walut
         {
-			if(Picker.SelectedIndex >=0 && Picker2.SelectedIndex >= 0 && Value.Text != null && Value.Text !=string.Empty) 
+			if(Picker.SelectedIndex >=0 && Picker2.SelectedIndex >= 0 && Value.Text != null && Value.Text !=string.Empty) //sprawdzenie czy waluty zostały wybrane oraz czy ilość została wpisana
 			{
-                Currency.TextColor = Xamarin.Forms.Color.Blue;
-				Currency.Text = "Zamiana walut, proszę czekać";
+                Currency.TextColor = Xamarin.Forms.Color.Blue; //zmiana koloru label'a na niebieski
+				Currency.Text = "Zamiana walut, proszę czekać"; //wypisanie statusu
 				Currency.FontSize = 25;
-                string valut1 = Waluty[Picker.SelectedIndex];
-				string valut2 = Waluty[Picker2.SelectedIndex];
-				string value = Value.Text;
-				string request = "https://api.apilayer.com/exchangerates_data/convert?to="+valut2+"&from="+valut1+"&amount="+value+"&apikey=HraVKaXnOFrP15XQkrOpGgSLiJxqKdM7";
-				try
+                string valut1 = Waluty[Picker.SelectedIndex]; //ustawienie pierwszej waluty
+				string valut2 = Waluty[Picker2.SelectedIndex]; //ustawienie drugiej waluty
+				string value = Value.Text; //ustawienie ilosci
+				string request = "https://api.apilayer.com/exchangerates_data/convert?to="+valut2+"&from="+valut1+"&amount="+value+"&apikey=HraVKaXnOFrP15XQkrOpGgSLiJxqKdM7"; //Tworzenie zapytania do API
+				try //Blok try catch do obsłużenia wyjątku w wypadku np. Braku dostępu do internetu lub przerwie w działaniu API
 				{
-                    HttpClient client = new HttpClient();
-                    var json = await client.GetStringAsync(request);
-                    Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(json);
+                    HttpClient client = new HttpClient(); //Utworzenie obiektu HttpClient
+                    var json = await client.GetStringAsync(request); //Pobranie JSON'a z API do zmiennej
+                    Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(json); //Deserializacja JSON'a do obiektu
 
-                    Currency.TextColor = Xamarin.Forms.Color.Black;
+                    Currency.TextColor = Xamarin.Forms.Color.Black; 
                     Currency.FontSize = 40;
-                    Currency.Text = Math.Round(myDeserializedClass.result, 2).ToString() + " " + valut2;
+                    Currency.Text = Math.Round(myDeserializedClass.result, 2).ToString() + " " + valut2; //Wypisanie wyniku
                 }
-				catch (Exception)
+				catch (Exception) //obsługa wyjątków
 				{
                           
                     Currency.FontSize = 40;
                     Currency.TextColor = Xamarin.Forms.Color.Red;
-                    Currency.Text = "Błąd połączenia!";
+                    Currency.Text = "Błąd połączenia!"; //ustawienie statusu jako błąd połączenia
                 }
                 		
             }
@@ -63,13 +63,13 @@ namespace Projekt
 			{
                 Currency.FontSize = 40;
                 Currency.TextColor = Xamarin.Forms.Color.Red;
-				Currency.Text = "Nie podano walut!";
+				Currency.Text = "Nie podano walut!"; //Ustawienie statusu na bład nie podanych walut
             }
 			else if(Value.Text == null || Value.Text == string.Empty)
 			{
                 Currency.FontSize = 40;
                 Currency.TextColor = Xamarin.Forms.Color.Red;
-                Currency.Text = "Nie podano ilosci!";
+                Currency.Text = "Nie podano ilosci!"; //Ustawienie statusu na bład nie podanej ilości
             }
 
         }
